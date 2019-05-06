f21ff606a1abddb5cf0242f07bd9e7b33ac01a52
William rohr <43350591+WillRohr@users.noreply.github.com>
2019-05-06 13:06:39 -0400

    Add files via upload

---
+++ src/simplegame/Chungus.java
@@ -0,0 +1,12 @@
+/*
+ * MAIN CHARACTER 1
+ */
+package simplegame;
+
+/**
+ *
+ * @author 805598
+ */
+public class Chungus {
+    
+}
---
+++ src/simplegame/Shrek.java
@@ -0,0 +1,12 @@
+/*
+ * MAIN CHARACTER 2
+ */
+package simplegame;
+
+/**
+ *
+ * @author 805598
+ */
+public class Shrek {
+    
+}
